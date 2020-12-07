5752a4786c26585ad72e35fbb12d0d937e68d789
LJ146 <73682859+LJ146@users.noreply.github.com>
2020-12-06 19:26:28 -0500

    Add files via upload

---
+++ config_B.js
@@ -0,0 +1,54 @@
+var config = {
+    style: 'mapbox://styles/vvv5/ckidlidfq2se11akum7jk5sb7',
+    accessToken: 'pk.eyJ1IjoidnZ2NSIsImEiOiJja2dzamx6YmgxMmQ3MnBtbHUzODd2amxyIn0.SdpSPxtxy0v3tjffgSmp4A',
+    showMarkers: false,
+    theme: 'light',
+    title: '',
+    subtitle: '',
+    byline: '',
+    footer: '',
+    chapters: [
+        {
+            id: '1',
+            alignment: 'right',
+            title: '',
+            image: '',
+            description: '',
+            location: {
+                center: [-74.00063, 40.65085],
+                zoom: 13.43,
+                pitch: 0.00,
+                bearing: 0.00
+            },
+            onChapterEnter: [
+                {
+                    layer: 'close_all',
+                    opacity: 0
+                }
+            ],
+            onChapterExit: [
+            ]
+        },
+        {
+            id: '2',
+            alignment: 'right',
+            title: '',
+            image: '',
+            description: '',
+            location: {
+                center: [-74.00063, 40.65085],
+                zoom: 13.43,
+                pitch: 0.00,
+                bearing: 0.00
+            },
+            onChapterEnter: [
+                {
+                    layer: 'close_all',
+                    opacity: 1
+                }
+            ],
+            onChapterExit: [
+            ]
+        }
+    ]
+};
